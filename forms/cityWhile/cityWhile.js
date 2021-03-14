416400e42d6a8f1ddb58a3e905d6fa8ee4952a05
Donovan McGuire <donovanjmcguire@gmail.com>
2021-03-14 17:29:21 -0500

    Update cityWhile.js

--- forms/cityWhile/cityWhile.js
+++ forms/cityWhile/cityWhile.js
@@ -1,13 +1,16 @@
+/*
 let cities = []
 let i = 0
 let doneWithCities = false
 while(doneWithCities == false){
     cities[i] = prompt('Enter the next city:')
     i++
-    doneWithCities = confirm("Are you done with the names?")
-    while(doneWithCities == true){
-        lc_cities =  cities.forEach( element => console.log(element.toLowerCase())
-        lc_cities.forEach( element => console.log(element))
-        }
+    doneWithCities = confirm("Are you done with entering cities?")
+
     }
 
+while ( i < cities.length){
+    console.log(cities.values().toLowerCase())
+    i++
+    }
+*/
