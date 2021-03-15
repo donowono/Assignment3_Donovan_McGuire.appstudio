12095e58ab0e0ae05327c3c87facf05939968555
Donovan McGuire <donovanjmcguire@gmail.com>
2021-03-14 20:20:56 -0500

    Update forReverseStates.js

--- forms/forReverseStates/forReverseStates.js
+++ forms/forReverseStates/forReverseStates.js
@@ -1,1 +1,5 @@
+
 states = [" Nebraska", "Iowa", "Minnesota", "South Dakota", "North Dakota"," Kansas", "Oklahoma", "Texas"]
+for( i = states.length - 1;  i  >= 0;  i--) {
+    console.log(states[i])
+}
