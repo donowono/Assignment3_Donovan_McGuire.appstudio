a29ec41e199417f623215d0e739ccaa2efa0c7cf
Donovan McGuire <donovanjmcguire@gmail.com>
2021-03-14 20:21:40 -0500

    Update dogStore.js

--- forms/dogStore/dogStore.js
+++ forms/dogStore/dogStore.js
@@ -1,10 +1,13 @@
 
-let products = [["Crocheted Dog, Toy", "$10.00"], ["Slow Feeder Bowl", "$14.00"] , ["Pawz Grey Treat Jar", "$20.00",] , ["Blue Sweet Dreams Thermal Pajamas", "$30.00"] ,  ["Checker Chewy Vuiton Bowl", "$32.00"] ,  ["Red Heart Sweater Dress", "$27.00"] , ["Velvet Hair Bow - Sun Kissed", "$12.00"] ,  ["Into the Wild Leash", "$27.00"]]
+let storeProducts = ['Crocheted Dog Toy, $10.00', 'Slow Feeder Bowl, $14.00', 'Pawz Grey Treat Jar, $20.00', 'Blue Sweet Dreams Thermal Pajamas, $30.00', 'Checker Chewy Vuiton Bowl, $32.00', 'Red Heart Sweater Dress, $27.00', 'Velvet Hair Bow - Sun Kissed, $12.00', 'Into the Wild Leash, $27.00']
+let wishList  = []
 
 
-btn1.onclick=function(){
-  for (i = 0; i  < products.length; i++){
-      lbl1.value = products[i]
+sel1.onchange = function(){
+  lbl1.textContent = storeProducts.value
 }
+
+btn3.onclick = function(){
+    wishList.push(sel1.value)
+    lblWish.textContent = wishList
 }
-customerList = inpt1.value
