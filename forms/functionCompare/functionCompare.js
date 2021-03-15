e7d10b7c4d64ebf3c00de2bd7bc5df2c8554bcda
Donovan McGuire <donovanjmcguire@gmail.com>
2021-03-14 20:20:52 -0500

    Create functionCompare.js

---
+++ forms/functionCompare/functionCompare.js
@@ -0,0 +1,15 @@
+/*
+pass1 = prompt('Enter your password:')
+pass2 = prompt('Enter your password again:')
+
+function compare(pass1, pass2){
+    if (pass1.localeCompare(pass2) == 0){
+        console.log('The passwords are the same')
+        return true
+    } else {
+        console.log("The passwords are not the same")
+        return false
+        }
+    }
+compare(pass1, pass2)
+*/
